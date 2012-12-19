38dea11fc437b4da22fe72a5d3fb621a89210513
Markus Klemm <markus@markusklemm.net>
2012-12-19 18:16:46 +0100

    3. fertig

--- U09.py
+++ U09.py
@@ -3,8 +3,22 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
+import scipy.integrate
 
 """Übung 09 Integrieren mit numpy"""
 #Markus Klemm WS12/13 Phy-BA
 
+funktion = lambda x: (np.exp(x/10)*np.sin(x) + x) / (np.log(x**2 + 10))
+ugrenze = 0
+ogrenze = 10
 
+y10 = np.array(funktion(np.linspace(ugrenze,ogrenze,10)))
+y100 = np.array(funktion(np.linspace(ugrenze,ogrenze,100)))
+y1000 = np.array(funktion(np.linspace(ugrenze,ogrenze,1000)))
+
+#3.
+print "Scipy Resultate:"
+print "Scipy.Integrate.Quad: " + str(scipy.integrate.quad(funktion,ugrenze,ogrenze))
+print "Scipy.Integrate.Trapz: " + str(scipy.integrate.trapz(y1000,dx=0.01))
+print "Scipy.Integrate.Simps: " + str(scipy.integrate.simps(y1000,dx=0.01))
